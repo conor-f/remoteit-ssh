c11ed93fb9528773629a5ccf74cb1578b92b14ac
Conor Flynn <conor-f@users.noreply.github.com>
2023-03-23 11:34:29 +0000

    Supports Config File + Profiles (0.2.2) (#2)
    * Adds config file and profile options.
    
    * Supports config file.
    
    * Adds README.md.
    
    * Adds README.md.
    
    * Lints.

--- src/remoteit_ssh/client.py
+++ src/remoteit_ssh/client.py
@@ -1,4 +1,5 @@
 import argparse
+import configparser
 import json
 import os
 import requests
@@ -8,11 +9,7 @@ from requests_http_signature import HTTPSignatureAuth
 from base64 import b64decode
 
 
-key_id = os.environ.get("R3_ACCESS_KEY_ID", None)
-key_secret_id = os.environ.get("R3_SECRET_ACCESS_KEY", None)
-
-
-def run_query(body):
+def run_query(body, key_id, key_secret_id):
     host = "api.remote.it"
     url_path = "/graphql/v1"
     content_type_header = "application/json"
@@ -44,13 +41,13 @@ def run_query(body):
     )
 
     if response.status_code == 403:
-        print("Incorrect auth. Check your R3_* environment variables.")
+        print("Incorrect auth")
         sys.exit(1)
 
     return response
 
 
-def get_device_details_from_device_name(device_name):
+def get_device_details_from_device_name(device_name, key_id, key_secret_id):
     response = run_query(
         {
             "query": f"""
@@ -68,17 +65,21 @@ query {{
         }}
     }}
 }}"""
-        }
+        },
+        key_id,
+        key_secret_id,
     )
 
     return response.json()["data"]["login"]["devices"]["items"]
 
 
-def get_ssh_details_from_device_name(device_name):
-    device_details = get_device_details_from_device_name(device_name)
+def get_ssh_details_from_device_name(device_name, key_id, key_secret_id):
+    device_details = get_device_details_from_device_name(
+        device_name, key_id, key_secret_id
+    )
 
     if not device_details:
-        print(f"Device matching {device_name} not found. Exiting.")
+        print(f"Device matching {device_name} not found")
         sys.exit(1)
 
     remote_id = device_details[0]["services"][0]["id"]
@@ -95,10 +96,13 @@ mutation {{
         port
     }}
 }}"""
-        }
+        },
+        key_id,
+        key_secret_id,
     )
 
     response = response.json()
+
     if "errors" in response:
         print("Error while trying to get device details:")
 
@@ -118,20 +122,69 @@ def parse_args():
         description="Matches a partial device name on Remoteit and opens an SSH connection to it."
     )
 
+    parser.add_argument(
+        "-f",
+        "--file",
+        default="~/.config/remoteit_ssh/config.ini",
+        help="Filepath for a config file to use",
+    )
+
+    parser.add_argument(
+        "-p",
+        "--profile",
+        default="default",
+        help="Which profile to use. Must match a section in your config file.",
+    )
+
     parser.add_argument("device_name")
 
     return parser.parse_args()
 
 
-def main():
-    if not key_id or not key_secret_id:
-        print(
-            "You must set your env varialbes of R3_ACCESS_KEY_ID and R3_SECRET_ACCESS_KEY!"
-        )
+def get_from_config_file(filepath, profile, option):
+    config = configparser.ConfigParser().read(filepath)
+
+    if profile not in config.sections():
+        print(f"Profile {profile} not found in config file {filepath}")
         sys.exit(1)
 
+    return config.get(profile, option, fallback=None)
+
+
+def get_key_id(args):
+    key_id = get_from_config_file(args.file, args.profile, "R3_ACCESS_KEY_ID")
+
+    if not key_id:
+        print("Couldn't parse key_id.")
+        sys.exit(1)
+
+    return key_id
+
+
+def get_key_secret_id(args):
+    key_secret_id = get_from_config_file(
+        args.file, args.profile, "R3_SECRET_ACCESS_KEY"
+    )
+
+    if not key_secret_id:
+        print("Couldn't parse key_secret_id.")
+        sys.exit(1)
+
+    return key_secret_id
+
+
+def main():
     args = parse_args()
-    details = get_ssh_details_from_device_name(args.device_name)
+
+    key_id = get_key_id(args)
+    key_secret_id = get_key_secret_id(args)
+    device_name = args.device_name
+
+    details = get_ssh_details_from_device_name(
+        device_name,
+        key_id,
+        key_secret_id,
+    )
 
     host = details["host"]
     port = details["port"]
